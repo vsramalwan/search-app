a5cbe212c2d68d0b3274f6ae5eeeb67d777d7491
Vinod <vsramalwan@gmail.com>
2022-04-27 20:38:57 +0200

    feat: setup express (#3)
    * feat: setup express
    
    * feat: dist folder

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,15 @@
-export {};
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.SERVER_PORT || 8001;
+
+app.get("/", (_req, res) => {
+  res.send("Express Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at https://localhost:${port}`);
+});
