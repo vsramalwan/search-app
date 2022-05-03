91a49d2b8aa58b965f2b35b0085952087924a1e4
Vinod <vsramalwan@gmail.com>
2022-05-03 20:48:08 +0200

    fix: search with check-boxes

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,22 @@ import { fetchCompanies } from "./server/search";
 
 function App() {
   const [companies, setCompanies] = useState([]);
+  const [bulldozer, setBulldozer] = useState(false);
+  const [compactor, setCompactor] = useState(false);
 
   const handleSearchSubmit = useCallback(
     async (searchTerm: string, isBulldozer: boolean, isCompactor: boolean) => {
+      // eslint-disable-next-line @typescript-eslint/await-thenable
       const companiesArray = await fetchCompanies(
         searchTerm,
         isBulldozer,
         isCompactor
       );
       setCompanies(companiesArray);
+      setBulldozer(isBulldozer);
+      setCompactor(isCompactor);
     },
-    [companies]
+    [companies, bulldozer, compactor]
   );
 
   const handleClearResults = useCallback(() => setCompanies([]), [companies]);
--- src/server/search.ts
+++ src/server/search.ts
@@ -1,26 +1,26 @@
-import _ from "lodash";
-
-export const fetchCompanies = _.memoize(
-  async (name, isBulldozer, isCompactor) => {
-    let res;
-    if (isBulldozer && isCompactor) {
-      res = await fetch(
-        `http://localhost:8001/companies?name=${name}&specialities=bulldozer&specialities=compactor`
-      );
-    } else if (!isBulldozer && isCompactor) {
-      res = await fetch(
-        `http://localhost:8001/companies?name=${name}&specialities=Compactor`
-      );
-    } else if (isBulldozer && !isCompactor) {
-      res = await fetch(
-        `http://localhost:8001/companies?name=${name}&specialities=Bulldozer`
-      );
-    } else if (!isBulldozer && !isCompactor) {
-      res = await fetch(`http://localhost:8001/companies?name=${name}`);
-    }
-    if (res?.status !== 200) return [];
-
-    const companies = await res.json();
-    return companies;
+export const fetchCompanies = async (
+  name: string,
+  isBulldozer: boolean,
+  isCompactor: boolean
+) => {
+  let res;
+  if (isBulldozer && isCompactor) {
+    res = await fetch(
+      `http://localhost:8001/companies?name=${name}&specialities=Bulldozer&specialities=Compactor`
+    );
+  } else if (!isBulldozer && isCompactor) {
+    res = await fetch(
+      `http://localhost:8001/companies?name=${name}&specialities=Compactor`
+    );
+  } else if (isBulldozer && !isCompactor) {
+    res = await fetch(
+      `http://localhost:8001/companies?name=${name}&specialities=Bulldozer`
+    );
+  } else if (!isBulldozer && !isCompactor) {
+    res = await fetch(`http://localhost:8001/companies?name=${name}`);
   }
-);
+  if (res?.status !== 200) return [];
+
+  const companies = await res.json();
+  return companies;
+};
